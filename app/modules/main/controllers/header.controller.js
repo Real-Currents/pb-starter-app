f930c8dedfca21b8dea3e233eb30c6e0ac111089
John Hall <jhall@maponics.com>
2016-10-15 18:24:55 -0400

    start to migrate to angular ui bootstrap

--- app/modules/main/controllers/header.controller.js
+++ app/modules/main/controllers/header.controller.js
@@ -2,9 +2,19 @@
 
   'use strict';
 
-  angular.module('app').controller('HeaderController', function($log) {
-
-
+  angular.module('app').controller('HeaderController', function($log, $scope, $window) {
+    var menu = this;
+    
+    this.isNavCollapsed = true;
+    this.isCollapsed = false;
+    this.isCollapsedHorizontal = false;
+    this.apply = function (event) { $scope.$apply(); };
+    this.close = function (event) { menu.isNavCollapsed = true; };
+    $window.addEventListener('resize', function (event) {
+        menu.close(event);
+        menu.apply(event);
+    });
+    return this;
   });
 
 })();
